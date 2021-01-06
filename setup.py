16f9d0933eba072ccea4248deb5f6cea82b441fc
eRajsh <rajesh.prabhu@gmail.com>
2021-01-06 19:07:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="Rajesh Prabhu",
     author_email="",
     name='textcloud',
-    version='0.0.2',
+    version='0.0.3',
     cmdclass=versioneer.get_cmdclass(),
     url='https://github.com/eRajsh/text_cloud',
     description="A little text cloud generator, based on AMueller's excellent word cloud",
